7af4fc982570b43f0b5757617422c68e9e7ecb3e
Deveesh Shetty <deveeshshetty@gmail.com>
2023-03-20 21:54:55 +0530

    ADD: Sketch Images and display few of them for now

--- src/components/ArtGallery/ArtGallery.tsx
+++ src/components/ArtGallery/ArtGallery.tsx
@@ -1,9 +1,13 @@
+import { arts } from "@/lib/artgallery"
+import Image from "next/image"
+import { useEffect, useRef } from "react"
 import styles from "./ArtGallery.module.css"
 function ArtGallery() {
   return (
     <section className={styles.container}>
       <h1>Art Gallery</h1>
       <div className={styles.art_container}>
+        {/* <img src="https://source.unsplash.com/random?nature" alt="" />
         <img src="https://source.unsplash.com/random?nature" alt="" />
         <img src="https://source.unsplash.com/random?nature" alt="" />
         <img src="https://source.unsplash.com/random?nature" alt="" />
@@ -19,8 +23,17 @@ function ArtGallery() {
         <img src="https://source.unsplash.com/random?nature" alt="" />
         <img src="https://source.unsplash.com/random?nature" alt="" />
         <img src="https://source.unsplash.com/random?nature" alt="" />
-        <img src="https://source.unsplash.com/random?nature" alt="" />
-        <img src="https://source.unsplash.com/random?nature" alt="" />
+        <img src="https://source.unsplash.com/random?nature" alt="" /> */}
+
+        {arts.map((art) => (
+          <Image
+            src={art.img}
+            alt={art.alt}
+            key={art.id}
+            width={1000}
+            height={1000}
+          />
+        ))}
       </div>
     </section>
   )
---
+++ src/lib/artgallery.ts
@@ -0,0 +1,59 @@
+import { v4 as uuid } from "uuid"
+
+export const arts = [
+  {
+    id: uuid(),
+    img: "/sketch/LalBaugChaRaja.jpg",
+    alt: "A pencil portrait of Lal Baug Cha Raja Ganapathi in Maharashtra",
+  },
+  {
+    id: uuid(),
+    img: "/sketch/Adiyogi.jpg",
+    alt: "Marker Drawing Adi Yogi Shiva",
+  },
+  {
+    id: uuid(),
+    img: "/sketch/Adiyogi.jpg",
+    alt: "Marker Drawing Adi Yogi Shiva",
+  },
+  {
+    id: uuid(),
+    img: "/sketch/Adiyogi.jpg",
+    alt: "Marker Drawing Adi Yogi Shiva",
+  },
+  {
+    id: uuid(),
+    img: "/sketch/Adiyogi.jpg",
+    alt: "Marker Drawing Adi Yogi Shiva",
+  },
+  {
+    id: uuid(),
+    img: "/sketch/Adiyogi.jpg",
+    alt: "Marker Drawing Adi Yogi Shiva",
+  },
+  {
+    id: uuid(),
+    img: "/sketch/Adiyogi.jpg",
+    alt: "Marker Drawing Adi Yogi Shiva",
+  },
+  {
+    id: uuid(),
+    img: "/sketch/Adiyogi.jpg",
+    alt: "Marker Drawing Adi Yogi Shiva",
+  },
+  {
+    id: uuid(),
+    img: "/sketch/Adiyogi.jpg",
+    alt: "Marker Drawing Adi Yogi Shiva",
+  },
+  {
+    id: uuid(),
+    img: "/sketch/Adiyogi.jpg",
+    alt: "Marker Drawing Adi Yogi Shiva",
+  },
+  {
+    id: uuid(),
+    img: "/sketch/Adiyogi.jpg",
+    alt: "Marker Drawing Adi Yogi Shiva",
+  },
+]
