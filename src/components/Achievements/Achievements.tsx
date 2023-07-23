573dfbce1384b65ae9515cfc5fe49ddfb3bdb9af
Deveesh Shetty <deveeshshetty@gmail.com>
2023-07-23 12:42:29 +0530

    Adds My recent Git and GitHub workshop

--- src/components/Achievements/Achievements.tsx
+++ src/components/Achievements/Achievements.tsx
@@ -3,59 +3,88 @@ import styles from "./Achievements.module.css"
 import Startupreneur from "../../assets/images/startupreneur.jpeg"
 import SOSC from "../../assets/images/SOSC.jpeg"
 import ThaniyaTech from "../../assets/images/ThaniyaTech.jpeg"
+import CommitToSuccess from "../../assets/achievements/Commit-to-Success.jpeg"
+import AchievementTile from "@/components/Achievements/AchievementTile"
 
 function Achievements() {
   return (
     <section className={styles.container}>
       <div className={styles.heading}>
-        <h2>Achievements and Experience</h2>
-        <h3>My achievements and experience in volutneering</h3>
+        <h2>Experience and Achievements</h2>
       </div>
       <div className={styles.card_container}>
-        <div className={styles.card}>
-          <Image src={Startupreneur} alt="Startupreneur Logo" />
-          <div className={styles.card_body}>
-            <div>
-              <h2 className={styles.card_title}>
-                Developer Intern at Startupreneur
-              </h2>
-              <p>Sep 2022 - Jan 2023</p>
+        <AchievementTile
+          image={Startupreneur}
+          orgName="The Startupreneur"
+          role="Developer Intern"
+          tenure="Sep 2022 - Jan 2023"
+          description="Worked on frontend for two e-commerce websites and built a single
+          page application. Gained experience in working with a team, how to
+          manage the tasks. Built my skills in frontend technologies and
+          also gained in depth knowledge of Git and Github."
+          orgLink="https://thestartupreneur.co/"
+        />
+        <AchievementTile
+          image={ThaniyaTech}
+          orgName="Thaniya Technologies"
+          role="Technical Trainer"
+          tenure="Apr 2022 - Jun 2023"
+          description="Taught Web Development with React and also basics of Full Stack
+          Application by building projects using MongoDB Atlas and
+          ExpressJS."
+          orgLink="https://thaniyatech.com/"
+        />
+        <AchievementTile
+          image={SOSC}
+          orgName="Sahyadri Open Source Community (SOSC)"
+          role="President"
+          tenure="Jun 2023 - Present"
+          description="SOSC is a community driven by Tech Enthusiasts, where our prime
+          goal is to learn and share our knowledge to other members, and
+          guide other students in the technical fields by building projects,
+          conducting workshops and hackathons."
+          orgLink="https://sosc.org.in/"
+        />
+      </div>
+      <div className={styles.heading}>
+        <h2>Activities and Interests</h2>
+        <div className={styles.activity_container}>
+          <div className={styles.activity_card}>
+            <Image
+              src={CommitToSuccess}
+              alt="Commit To Success - Workshop on Git and GitHub"
+            />
+            <div className={styles.activity_card_body}>
+              <p>
+                Sahyadri Open Source Community (SOSC) organized a workshop
+                entitled “Commit to Success” - a workshop detailing Git and
+                GitHub. The workshop was headed by Deveesh Shetty, President of
+                SOSC.
+              </p>
+              <p>
+                The event commenced with a short introduction to Git and GitHub
+                and in what ways they are different from each other. Among
+                points of discussion were: How these tools help in effective
+                collaboration between coders and developers, how Git practices
+                version control and how these platforms are flag-bearers of
+                open-source.
+              </p>
+              <p>
+                This event covered topics like, "add-commit-push", working in
+                branches, and to send a pull request and contribute to others
+                project in GitHub.
+              </p>
+              <p>
+                Checkout the blog about Git and GitHub -{" "}
+                <a
+                  href="https://dev.to/devshetty/git-and-github-for-beginners-the-basics-d3n"
+                  className={styles.link}
+                  target="_blank"
+                >
+                  Here
+                </a>
+              </p>
             </div>
-            <p>
-              Worked on frontend for two e-commerce websites and built a single
-              page application. Gained experience in working with a team, how to
-              manage the tasks. Built my skills in frontend technologies and
-              also gained in depth knowledge of Git and Github.
-            </p>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <Image src={ThaniyaTech} alt="Startupreneur Logo" />
-          <div className={styles.card_body}>
-            <div>
-              <h2 className={styles.card_title}>Technical Trainer</h2>
-              <p>Apr 2023 - June 2023</p>
-            </div>
-            <p>
-              Taught Web Development with React and also basics of Full Stack
-              Application by building projects using MongoDB Atlas and
-              ExpressJS.
-            </p>
-          </div>
-        </div>
-        <div className={styles.card}>
-          <Image src={SOSC} alt="SOSC Logo" />
-          <div className={styles.card_body}>
-            <div>
-              <h2 className={styles.card_title}>President</h2>
-              <p>June 2023 - Present</p>
-            </div>
-            <p>
-              SOSC is a community driven by Tech Enthusiasts, where our prime
-              goal is to learn and share our knowledge to other members, and
-              guide other students in the technical fields by building projects,
-              conducting workshops and hackathons
-            </p>
           </div>
         </div>
       </div>
