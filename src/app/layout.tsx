f79f5cefc5f6deae80731b713cb9764b3c1706d6
Deveesh Shetty <deveeshshetty@gmail.com>
2023-10-06 09:52:14 +0530

    Adds SEO and generalizes social link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,54 @@ import { Metadata } from "next"
 import "@/app/globals.css"
 import Navbar from "@/components/navbar"
 import { Toaster } from "@/components/ui/toaster"
+import { siteConfig } from "@/lib/config"
 
 export const metadata: Metadata = {
-  title: "Deveesh Shetty",
-  description: "Portfolio Site of Deveesh Shetty",
+  title: {
+    default: "Deveesh <Shetty></Shetty>",
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: ["Deveesh Shetty", "Frontend Developer", "React", "Next.js"],
+  creator: "Deveesh-Shetty",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+    creator: "@ShettyDeveesh",
+  },
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  authors: [
+    {
+      name: "Deveesh Shetty",
+      url: siteConfig.url,
+    },
+  ],
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
 }
 
 export default function RootLayout({
